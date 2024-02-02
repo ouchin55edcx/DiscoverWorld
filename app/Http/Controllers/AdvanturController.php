ab1896e7ed9957b46c7169d1dd0465fef275dbd7
ouchin55edcx <ouchinmustapha82@gmail.com.com>
2024-02-02 22:32:41 +0100

    cache

--- app/Http/Controllers/AdvanturController.php
+++ app/Http/Controllers/AdvanturController.php
@@ -24,7 +24,13 @@ class AdvanturController extends Controller
     
         $adventuresPerDestination = Destination::withCount('adventures')->get();
 
-
+        $limitedAdventuresWithDestinationAndLimitedPhotos =
+            cache()->remember(
+                "adventure_id",
+                30,
+                fn () =>
+                $limitedAdventuresWithDestinationAndLimitedPhotos
+            );
         return view('welcome', [
             "adventures" => $limitedAdventuresWithDestinationAndLimitedPhotos,
             'totalAdventures' => $totalAdventures,
